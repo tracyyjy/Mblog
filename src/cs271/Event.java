cf37da7ad868785118f93f0d0cebba1f7b971461
Yifan Shen <shensahib@gmail.com>
2013-11-15 00:12:16 -0800

    Files inherited from HW1 added.

---
+++ src/cs271/Event.java
@@ -0,0 +1,22 @@
+package cs271;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Photeinis
+ * Date: 10/21/13
+ * Time: 10:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Event {
+    public int time;
+    public int nodeId;
+    public String key;
+    public String op;
+
+    public Event(String Key, String Op, int Time, int Node){
+        time = Time;
+        nodeId = Node;
+        key = Key;
+        op = Op;
+    }
+}
---
+++ src/cs271/Message.java
@@ -0,0 +1,21 @@
+package cs271;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Photeinis
+ * Date: 10/21/13
+ * Time: 10:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Message {
+    int sourceReplicaId;
+    int destReplicaId;
+    ArrayList<Event> NP = new ArrayList<Event>();
+    int[][] TimeTable = new int[3][3];
+    public Message(int s, int d){
+        sourceReplicaId = s;
+        destReplicaId = d;
+    }
+}
