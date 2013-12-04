de344ff2b8a8c1f8603b176767d84e01ca4b2149
Yifan Shen <shensahib@gmail.com>
2013-12-04 05:00:26 -0800

    Nov 30, ec2 setup, logic fix

--- src/cs271/Node.java
+++ src/cs271/Node.java
@@ -375,6 +375,8 @@ public class Node {
                     tmpBallotNumber += cluster.size();
                 currentProposedBallotNumbers.put(position, tmpBallotNumber);
                 propose(proposal.getValue(), proposal.getPosition());
+                log( "REPROPOSED!");
+
                 return;
             }
 
@@ -387,7 +389,7 @@ public class Node {
                     maxAcceptedProposalBallotNumber.put(position,-1);
                 }
                 if (acceptedProposal.getBallotNumber() > maxAcceptedProposalBallotNumber.get(position)){
-                    proposal = acceptedProposal;
+                    proposal.setValue(acceptedProposal.getValue());
                     maxAcceptedProposalBallotNumber.put(position, proposal.getBallotNumber());
                 }
             }
